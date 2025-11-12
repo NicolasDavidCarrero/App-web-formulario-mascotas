c9a975d1eb689bd400f7a86b48819d6820794f25
NicolasDavidCarrero <ndcarreror@udistrital.edu.co>
2025-11-12 13:31:41 -0500

    mascota.py

---
+++ mascota.py
@@ -0,0 +1,15 @@
+from db_config import db 
+
+class Mascota(db.Model): #usar mayusculas para las clases
+    _tablename_="mascota"
+
+    id=db.Column(db.Integer,primary_key=True)
+    nombre=db.Column(db.String(50))
+    raza=db.Column(db.String(70))
+    edad=db.Column(db.String(15))
+
+    def __init__(self,nombre, raza, dueño, edad):
+        self.nombre=nombre
+        self.raza= raza
+        self.dueño=dueño
+        self.edad= edad
